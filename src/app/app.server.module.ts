c00a7b6f46dd9d37f365360936dfe10785f844f1
Andrew Schwartz <aschwartz5352@gmail.com>
2023-04-25 21:07:10 -0500

    delete service key

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,10 +3,10 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import * as admin from 'firebase-admin';
-import { FIREBASE_ADMIN } from './app.module';
+// import * as admin from 'firebase-admin';
+// import { FIREBASE_ADMIN } from './app.module';
 
-let serviceAccount = require("./../serviceAccount.json");
+// let serviceAccount = require("./../serviceAccount.json");
 
 @NgModule({
   imports: [
@@ -15,10 +15,10 @@ let serviceAccount = require("./../serviceAccount.json");
   ],
   bootstrap: [AppComponent],
   providers: [
-    { provide: FIREBASE_ADMIN, useFactory: () => admin.apps[0] || admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: "https://fibal-app.firebaseio.com"
-      }) }
+    // { provide: FIREBASE_ADMIN, useFactory: () => admin.apps[0] || admin.initializeApp({
+    //     credential: admin.credential.cert(serviceAccount),
+    //     databaseURL: "https://fibal-app.firebaseio.com"
+    //   }) }
 ],
 })
 export class AppServerModule {}
